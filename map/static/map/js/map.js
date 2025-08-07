15d38c7e797d269d870d4be7b28078d00645088a
DemevengDerrick <demeveng@gmail.com>
2025-08-07 21:20:12 +0300

    added chatbot

--- map/static/map/js/map.js
+++ map/static/map/js/map.js
@@ -5,3 +5,33 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
+// chatbot code
+
+const messageList = document.querySelector('.message-list')
+const messageForm = document.querySelector('.message-form')
+const messageInput = document.querySelector('.message-input')
+
+messageForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    const message = messageInput.value.trim()
+    if (message === 0){
+        return;
+    }
+    console.log(message)
+    const messageItem = document.createElement('li');
+    messageItem.classList.add('list-group-item')
+    messageItem.innerHTML = `
+                        <div>
+                            <b>You</b>
+                        </div>
+                        <div>
+                            ${message}
+                        </div>`;
+    
+    messageList.appendChild(messageItem);
+    messageInput.value = '';
+});
+
+// Send message to the backend to be processed by ChatGPT
+
